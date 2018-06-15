294d0ad5ed004ca45b3e2c0a9a30a41d64b3533a
Ethan <w43wei@uwaterloo.ca>
2018-06-15 13:33:58 -0400

    add option view

--- app/src/main/java/ca/uwaterloo/cs446/cs446project/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/cs446/cs446project/MainActivity.java
@@ -48,6 +48,13 @@ public class MainActivity extends Activity implements Observer{
             }
         });
 
+        optionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, OptionActivity.class));
+            }
+        });
+
     }
 
     @Override
--- app/src/main/java/ca/uwaterloo/cs446/cs446project/OptionActivity.java
+++ app/src/main/java/ca/uwaterloo/cs446/cs446project/OptionActivity.java
@@ -1,13 +1,32 @@
 package ca.uwaterloo.cs446.cs446project;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class OptionActivity extends AppCompatActivity {
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_option);
+
+        backButton=findViewById(R.id.optionBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OptionActivity.this, MainActivity.class));
+            }
+        });
     }
 }
