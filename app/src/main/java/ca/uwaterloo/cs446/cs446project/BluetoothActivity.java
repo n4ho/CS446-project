908e92b342996982ada8acee28829ce025db6f2a
Natalie Ho <natalieho@v1020-wn-8-40.campus-dynamic.uwaterloo.ca>
2018-06-15 15:56:25 -0400

    start

--- app/src/main/java/ca/uwaterloo/cs446/cs446project/BluetoothActivity.java
+++ app/src/main/java/ca/uwaterloo/cs446/cs446project/BluetoothActivity.java
@@ -9,7 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class bluetooth extends Activity {
+public class BluetoothActivity extends Activity {
 
     Button On;
 
