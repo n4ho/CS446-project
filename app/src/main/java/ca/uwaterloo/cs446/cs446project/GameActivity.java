8539892ce4be139f96841c4690c67bfcd09d413f
Ethan <w43wei@uwaterloo.ca>
2018-06-28 17:02:38 -0400

    add music service, need test

--- app/src/main/java/ca/uwaterloo/cs446/cs446project/GameActivity.java
+++ app/src/main/java/ca/uwaterloo/cs446/cs446project/GameActivity.java
@@ -1,8 +1,13 @@
 package ca.uwaterloo.cs446.cs446project;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
@@ -16,6 +21,33 @@ public class GameActivity extends Activity implements Observer{
 
     GameModel model;
 
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class), Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +60,38 @@ public class GameActivity extends Activity implements Observer{
         setContentView(new GameView(this, getWindowManager().getDefaultDisplay(), model));
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         GameModel.setInstance(this, getWindowManager().getDefaultDisplay(), 60, true);
+
+        // bind music service to activity
+        doBindService();
+        // start service
+        Intent music = new Intent();
+        music.setClass(this,MusicService.class);
+        startService(music);
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //mServ.pauseMusic();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //mServ.resumeMusic();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        doUnbindService();
     }
 
     @Override
