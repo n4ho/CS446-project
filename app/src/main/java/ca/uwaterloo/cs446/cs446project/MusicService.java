d38e604b683b26a8d90dac3425eb0d555730871d
Ethan <w43wei@uwaterloo.ca>
2018-06-28 17:02:30 -0400

    add music service, need test

---
+++ app/src/main/java/ca/uwaterloo/cs446/cs446project/MusicService.java
@@ -0,0 +1,11 @@
+package ca.uwaterloo.cs446.cs446project;
+
+import android.app.Service;
+import android.media.MediaPlayer;
+
+/**
+ * Created by ethan on 2018-06-28.
+ */
+
+public class MusicService extends Service implements MediaPlayer.OnErrorListener {
+}
